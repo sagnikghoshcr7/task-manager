c9b9248e82ba65ec99893d2700e42c0b69475466
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-08 02:29:16 +0530

    mongo server connect

---
+++ mongodb.js
@@ -0,0 +1,15 @@
+// CRUD operations
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+  if (error) {
+    return console.log('Unable to connect to database!')
+  }
+
+  console.log('Connected correctly!')
+})
