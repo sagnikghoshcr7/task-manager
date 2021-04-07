ecd051a2f3707eddf640088b4a1415ffcd366b53
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-08 03:50:48 +0530

    add search method

--- mongodb.js
+++ mongodb.js
@@ -5,10 +5,6 @@ const { MongoClient, ObjectID } = require('mongodb')
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-const id = new ObjectID()
-console.log(id)
-console.log(id.getTimestamp())
-
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
   if (error) {
     return console.log('Unable to connect to database!')
@@ -16,31 +12,11 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
   const db = client.db(databaseName)
 
-  db.collection('users').insertOne({
-    _id: id,
-    name: 'Sankha',
-    age: 20
-  }, (error, result) => {
+  db.collection('users').findOne({ name: 'Sankha', age: 1 }, (error, user) => {
     if (error) {
-      return console.log('Unable to insert user')
+      return console.log('Unable to fetch')
     }
 
-    console.log(result.ops)
+    console.log(user)
   })
-
-  // db.collection('users').insertMany([
-  //   {
-  //     name: 'Arnab',
-  //     age: 21
-  //   }, {
-  //     name: 'Sayantan',
-  //     age: 22
-  //   }
-  // ], (error, result) => {
-  //   if (error) {
-  //     return console.log('Unable to insert documents!')
-  //   }
-
-  //   console.log(result.ops)
-  // })
 })
