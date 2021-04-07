d864dfe39c383dc63e6e7fb0b5805d17eb9b5b84
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-08 03:28:19 +0530

    playing with functions to insert data

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,13 @@
 // CRUD operations
 
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID()
+console.log(id)
+console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
   if (error) {
@@ -14,7 +17,30 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
   const db = client.db(databaseName)
 
   db.collection('users').insertOne({
-    name: 'Sagnik',
+    _id: id,
+    name: 'Sankha',
     age: 20
+  }, (error, result) => {
+    if (error) {
+      return console.log('Unable to insert user')
+    }
+
+    console.log(result.ops)
   })
+
+  // db.collection('users').insertMany([
+  //   {
+  //     name: 'Arnab',
+  //     age: 21
+  //   }, {
+  //     name: 'Sayantan',
+  //     age: 22
+  //   }
+  // ], (error, result) => {
+  //   if (error) {
+  //     return console.log('Unable to insert documents!')
+  //   }
+
+  //   console.log(result.ops)
+  // })
 })
