3dd27003dafe197d12dbf8cdfda1da551446e53f
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-09 02:12:07 +0530

    crete task & user models

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,14 +5,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useCreateIndex: true,
 })
 
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  }
-})
