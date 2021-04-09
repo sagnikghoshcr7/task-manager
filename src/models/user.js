e2fd4977cec44124d583de3ce37e56651ce1b23d
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-10 02:11:29 +0530

    seperate task route

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
@@ -39,5 +39,7 @@ const User = mongoose.model('User', {
     }
   }
 })
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
