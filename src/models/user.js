9219c78a6a7d253caf49fa1cf024e05b41e150d0
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-10 03:29:15 +0530

    add bcrypt, find by credentials

--- src/models/user.js
+++ src/models/user.js
@@ -41,6 +41,23 @@ const userSchema = new mongoose.Schema({
   }
 })
 
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email })
+
+  if (!user) {
+    throw new Error('Unable to login')
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password)
+
+  if (!isMatch) {
+    throw new Error('Unable to login')
+  }
+
+  return user
+}
+
+// Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
   const user = this
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -13,6 +13,14 @@ router.post('/users', async (req, res) => {
   }
 })
 
+router.post('/users/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(req.body.email, req.body.password)
+  } catch (e) {
+    
+  }
+})
+
 router.get('/users', async (req, res) => {
   try {
     const users = await user.find({})
