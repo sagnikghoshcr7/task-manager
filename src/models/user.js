9d6f2f70e899ef8011222a258631d85c617eca3f
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-10 03:10:40 +0530

    update bug fixed

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -38,6 +39,16 @@ const userSchema = new mongoose.Schema({
       }
     }
   }
+})
+
+userSchema.pre('save', async function (next) {
+  const user = this
+
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8)
+  }
+
+  next()
 })
 
 const User = mongoose.model('User', userSchema)
