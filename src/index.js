7ae07b271dd69d6144651bf684f9884891ddd3d8
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-10 01:40:08 +0530

    seperate user route

--- src/index.js
+++ src/index.js
@@ -2,69 +2,13 @@ const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
 const Task = require('./models/task')
+const userRouter = require('./routers/user')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-
-app.post('/users', async (req, res) => {
-  const user = new User(req.body)
-
-  try {
-    await user.save()
-    res.status(201).send(user)
-  } catch (e) {
-    res.status(400).send(e)
-  }
-})
-
-app.get('/users', async (req, res) => {
-  try {
-    const users = await user.find({})
-    res.send(users)
-  } catch (e) {
-    res.status(500).send()
-  }
-})
-
-app.get('/users/:id',async (req, res) => {
-  const _id = req.params.id
-
-  try {
-    const user = await User.findById(_id)
-
-    if (!user) {
-      return res.status(404).send()
-    }
-
-    res.send(user)
-  } catch (e) {
-    res.status(500).send()
-  }
-})
-
-app.patch('/users/:id', async (req, res) => {
-  const updates = Object.keys(req.body)
-  const allowedUpdates = ['name', 'email', 'password', 'age']
-  const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-  if (!isValidOperation) {
-    return res.status(400).send({ error: 'Invalid updates!' })
-  }
-
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators:true })
-
-    if (!user) {
-      return res.status(404).send()
-    }
-
-    res.send(user)
-  } catch (e) {
-    res.status(400).send(e)
-  }
-})
+app.use(userRouter)
 
 app.post('/tasks', async (req, res) => {
   const task = new Task(req.body)
@@ -121,6 +65,20 @@ app.patch('./tasks/:id', async (req, res) => {
     res.send(task)
   } catch (error) {
     res.status(400).send(e)
+  }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id)
+
+    if (!task) {
+      res.status(404).send()
+    }
+
+    res.send(task)
+  } catch (e) {
+    res.status(500).send()
   }
 })
 
