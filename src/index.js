2880cfee1bf55807f3b98feeb25be75fa5c69f4a
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-10 00:22:33 +0530

    add updates_every

--- src/index.js
+++ src/index.js
@@ -47,6 +47,12 @@ app.get('/users/:id',async (req, res) => {
 app.patch('/users/:id', async (req, res) => {
   const updates = Object.keys(req.body)
   const allowedUpdates = ['name', 'email', 'password', 'age']
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' })
+  }
+
   try {
     const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators:true })
 
