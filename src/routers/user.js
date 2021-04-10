6634c134fb5074b683b6806d54740b3b3b0a2957
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-11 01:29:53 +0530

    user router bug fix

--- src/routers/user.js
+++ src/routers/user.js
@@ -52,23 +52,7 @@ router.get('/users/me', auth, async (req, res) => {
   res.send(req.user)
 })
 
-router.get('/users/:id',async (req, res) => {
-  const _id = req.params.id
-
-  try {
-    const user = await User.findById(_id)
-
-    if (!user) {
-      return res.status(404).send()
-    }
-
-    res.send(user)
-  } catch (e) {
-    res.status(500).send()
-  }
-})
-
-router.patch('/users/:id', async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
   const updates = Object.keys(req.body)
   const allowedUpdates = ['name', 'email', 'password', 'age']
   const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -78,31 +62,17 @@ router.patch('/users/:id', async (req, res) => {
   }
 
   try {
-    const user = await User.findById(req.params.id)
-
     updates.forEach((update) => user[update] = req.body[update])
-
-    await user.save()
-    // const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators:true })
-
-    if (!user) {
-      return res.status(404).send()
-    }
-
-    res.send(user)
+    await req.user.save()
+    res.send(req.user)
   } catch (e) {
     res.status(400).send(e)
   }
 })
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth, async (req, res) => {
   try {
-    const user = await User.findByIdAndDelete(req.params.id)
-
-    if (!user) {
-      return res.status(404).send()
-    }
-
+    await req.user.remove()
     res.send(user)
   } catch (e) {
     res.status(500).send()
