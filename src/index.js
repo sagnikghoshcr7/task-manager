a85d2ebddb76330b1de60805b22c09df80fda3ea
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-10 18:06:14 +0530

    add next method

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use((req, res, next) => {
+  console.log(req.method, req.path)
+  next()
+})
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
