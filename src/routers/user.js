63261d0fea8c9fe2b50ba51422502f88a4d2d2d2
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-12 03:53:47 +0530

    add upload avatar feature

--- src/routers/user.js
+++ src/routers/user.js
@@ -81,7 +81,6 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 const upload = multer({
-  dest: 'avatars',
   limits: {
     fileSize: 1000000
   },
@@ -94,7 +93,9 @@ const upload = multer({
   }
 })
 
-router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+router.post('/users/me/avatar', upload.single('avatar'), async (req, res) => {
+  req.user.avatar = req.file.buffer
+  await req.user.save()
   res.send()
 }, (error, req, res, next) => {
   res.status(400).send({ error: error.message })
