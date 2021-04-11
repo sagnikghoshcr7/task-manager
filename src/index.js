6751cdfe29aeaa88998593fdbe770a33f29a3d26
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-12 03:28:39 +0530

    restrict uploading file type

--- src/index.js
+++ src/index.js
@@ -11,6 +11,13 @@ const upload = multer({
   dest: 'images',
   limits: {
     fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      return cb(new Error('Please upload a Word Document'))
+    }
+
+    cb(undefined, true)
   }
 })
 app.post('/upload', upload.single('upload'), (req, res) => {
