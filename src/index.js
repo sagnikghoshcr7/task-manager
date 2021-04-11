1249c46e928f33d3d7abdc3d39e8983984f48d10
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-12 03:44:34 +0530

    add error cb to upload file feature

--- src/index.js
+++ src/index.js
@@ -20,8 +20,11 @@ const upload = multer({
     cb(undefined, true)
   }
 })
+
 app.post('/upload', upload.single('upload'), (req, res) => {
   res.send()
+}, (error, req, res, next) => {
+  res.status(400).send({ error: error.message })
 })
 
 app.use(express.json())
