1bd63e3940efe4e5559afc32c6a93c44ff37d6ed
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-11 21:19:49 +0530

    add upload feature

--- src/index.js
+++ src/index.js
@@ -6,17 +6,13 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//   if (req.method === 'GET') {
-//     res.send('GET requests are disabled')
-//   } else {
-//     next()
-//   }
-// })
-
-// app.use((req, res, next) => {
-//   res.status(503).send('Site is corrently down. Check back soon!')
-// })
+const multer = require('multer')
+const upload = multer({
+  dest: 'images'
+})
+app.post('/upload', upload.single('upload'), (req, res) => {
+  res.send()
+})
 
 app.use(express.json())
 app.use(userRouter)
@@ -25,14 +21,3 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log('Server is up on port' + port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-  const user = await User.findById('some_id_like_3sdf9sdf649sdg8s')
-  await user.populate('tasks').execPopulate()
-  console.log(user.tasks)
-}
-
-main()
