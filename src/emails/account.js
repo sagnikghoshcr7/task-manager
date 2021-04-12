dac1fda3e20a9a039130979dae3758342d5b6f85
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-04-12 19:13:37 +0530

    add send email feature

---
+++ src/emails/account.js
@@ -0,0 +1,28 @@
+const sgMail = require('@sendgrid/mail')
+
+const sendgridAPIKey = 'API_key_like_7g9v6s9fv87v09sv89s0v68f0'
+
+sgMail.setApiKey(sendgridAPIKey)
+
+const sendWelcomeEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: 'sagnikghoshmandra@gmail.com',
+    subject: 'Thanks for joining in!',
+    text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+  })
+}
+
+const sendCancelationEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: 'sagnikghoshmandra@gmail.com',
+    subject: 'Sorry to see you go!',
+    text: `Goodbye, ${name}. I hope to see you back sometime soon.`
+  })
+}
+
+module.exports = {
+  sendWelcomeEmail,
+  sendCancelationEmail
+}
